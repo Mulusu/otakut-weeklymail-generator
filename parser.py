00d14cf44212f877c92fddf6daa26206e097e9f9
Mur Susu <susu@tachikoma>
2017-08-21 14:50:38 +0300

    Bugfix

--- parser.py
+++ parser.py
@@ -3,8 +3,8 @@ from datetime import datetime
 
 
 class Event():
-    start = None
-    end = None
+    start = datetime(1980,1,1,1,1)
+    end = datetime(1980,1,1,1,1)
     topic = ""
     description = ""
 
@@ -16,13 +16,13 @@ class Parser():
         '''
         This parses the calendar ics data and creates the appropriate events to events list
         '''
-        day,month,year,weeknum = datetime.now().strftime('%d %m %Y %W').split(' ')
-        if(datetime.now().weekday() == 6): # On sundays we want to create NEXT week's mail, not the current one's
-            weeknum = str(int(weeknum) + 1)
+        year,weeknum = datetime.now().strftime('%Y %U').split(' ')
+#        if(datetime.now().weekday() == 6): # On sundays we want to create NEXT week's mail, not the current one's
+#            weeknum = str(int(weeknum) + 1)
         data = open(filename)
         line = data.readline().split(":",1)
         event = Event()
-        while(len(line) > 1):
+        while(len(line[0]) > 0):
             if(line[0].strip() == "BEGIN" and line[1].strip() == "VEVENT"):
                 event = Event()
 
@@ -30,6 +30,7 @@ class Parser():
             elif(line[0].strip() == "DTSTART" or line[0].strip() == "DTEND"):
                 kala = line[0]
                 date = datetime(int(line[1][0:4]),int(line[1][4:6]),int(line[1][6:8]),(int(line[1][9:11])+gmtoffset)%24,int(line[1][11:13]))
+                print(date)
                 if(kala == "DTSTART"):
                     event.start = date
                 else:
@@ -57,9 +58,6 @@ class Parser():
             line = data.readline().split(":",1)
 
         # Sort the list according to starting time (calendar saves them in the creation order, which isn't always the same)
-#        for x in self.events:
-#            for y in self.events:
-#                if(y.start < x.start):
         self.events.sort(key=lambda event:event.start)
 
         # Separate reservations from club's events
@@ -67,3 +65,6 @@ class Parser():
             if("reserved" in e.topic.lower() or "reservation" in e.topic.lower()):
                 self.reservations.append(e)
                 self.events.remove(e)
+
+        print(len(self.reservations))
+        print(len(self.events))
